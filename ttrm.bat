3c8ce36f5908e78eb1b64f43af4c7e575f3b4410
ronanwow1001 <ronancaleb123@gmail.com>
2019-07-04 14:03:12 -0700

    mindblown

---
+++ ttrm.bat
@@ -0,0 +1,26 @@
+@echo off
+
+title "Toontown Remodified Engine"
+
+set /P ttrUsername="Username: " || ^
+set ttrUsername=username
+set /P ttrpassword="Paassword: " || ^
+set ttrpassword=password
+set TTR_PLAYCOOKIE=%ttrUsername%
+set TTR_PASSWORD=%ttrpassword%
+set username=%ttrUsername%
+set password=%ttrpassword%
+set TTR_GAMESERVER=192.99.144.208
+
+rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
+set /P PPYTHON_PATH=<PPYTHON_PATH
+
+echo ===============================
+echo Starting Toontown Remodified - DEV...
+echo ppython: %PPYTHON_PATH%
+echo Username: %ttrUsername%
+echo Client Agent IP: %TTR_GAMESERVER%
+echo ===============================
+
+%PPYTHON_PATH% -m toontown.toonbase.ToontownStart
+pause
