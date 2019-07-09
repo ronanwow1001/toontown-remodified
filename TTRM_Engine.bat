084673394f6249d630f063c150a12edaddae819d
ronanwow1001 <ronancaleb123@gmail.com>
2019-07-09 11:17:16 -0700

    Update TTRM_Engine.bat

--- TTRM_Engine.bat
+++ TTRM_Engine.bat
@@ -3,13 +3,13 @@
 title Toontown Remodified Engine
 
 set /P ttrUsername="Username: " || ^
-set ttrUsername=username
-set /P ttrpassword="Password: " || ^
-set ttrpassword=password
-set TTR_PLAYCOOKIE=%ttrUsername%
-set TTR_PASSWORD=%ttrpassword%
-set username=%ttrUsername%
-set password=%ttrpassword%
+set ttrmUsername=username
+set /P ttrmpassword="Password: " || ^
+set ttrmpassword=password
+set TTRM_PLAYCOOKIE=%ttrmUsername%
+set TTRM_PASSWORD=%dev%
+set username=%ttrmUsername%
+set password=%ttrmpassword%
 set TTR_GAMESERVER=192.99.144.208
 
 rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
@@ -18,8 +18,8 @@ set /P PPYTHON_PATH=<PPYTHON_PATH
 echo ===============================
 echo Starting Toontown Remodified - DEV...
 echo ppython: %PPYTHON_PATH%
-echo Username: %ttrUsername%
-echo Client Agent IP: %TTR_GAMESERVER%
+echo Username: %ttrmUsername%
+echo Client Agent IP: %TTRM_GAMESERVER%
 echo ===============================
 
 %PPYTHON_PATH% -m "toontown.toonbase.Cilentstart.py"
